224ad8aa976eee934b9f4c724e617a346ff855d4
Elondo Mbonze <mbonze.elondo@gmail.com>
2016-01-03 21:29:54 +0000

    First commit

---
+++ test/decorators.js
@@ -0,0 +1,215 @@
+'use strict';
+
+import { assert } from 'chai';
+
+import * as config from '../lib/config';
+import { action, after, before, bodyParser, consumes, controller, get, post, produces, queryParser } from '../lib';
+
+describe('decorators', function () {
+  
+  describe('@after', function () {
+    describe('class', function () {
+      it(`should set the correct value for [${Symbol.keyFor(config.afterProp)}] property on class`, function () {
+
+        let clazz = class {};
+        let handlers = [() => {}, () => {}];
+        let fn = after(...handlers);
+
+        fn(clazz);
+
+        assert.deepEqual(handlers, clazz[config.afterProp]);
+      });
+    });
+    describe('method', function () {
+      it(`should set the correct value for [${Symbol.keyFor(config.afterProp)}] property on method`, function () {
+
+        let clazz = class {};
+        let descriptor = {
+          value: function () {}
+        };
+        let handlers = [() => {}, () => {}];
+        let fn = after(...handlers);
+
+        fn(clazz, 'any', descriptor);
+
+        assert.notProperty(clazz, config.afterProp);
+        assert.deepEqual(handlers, descriptor.value[config.afterProp]);
+      });
+    });
+  });  
+  
+  describe('@before', function () {
+    describe('class', function () {
+      it(`should set the correct value for [${Symbol.keyFor(config.beforeProp)}] property on class`, function () {
+
+        let clazz = class {};
+        let handlers = [() => {}, () => {}];
+        let fn = before(...handlers);
+
+        fn(clazz);
+
+        assert.deepEqual(handlers, clazz[config.beforeProp]);
+      });
+    });
+    describe('method', function () {
+      it(`should set the correct value for [${Symbol.keyFor(config.beforeProp)}] property on method`, function () {
+
+        let clazz = class {};
+        let descriptor = {
+          value: function () {}
+        };
+        let handlers = [() => {}, () => {}];
+        let fn = before(...handlers);
+
+        fn(clazz, 'any', descriptor);
+
+        assert.notProperty(clazz, config.beforeProp);
+        assert.deepEqual(handlers, descriptor.value[config.beforeProp]);
+      });
+    });
+  });   
+  
+  describe('@bodyParser', function () {
+    describe('class', function () {
+      it(`should set the correct value for [${Symbol.keyFor(config.bodyParserProp)}] property on class`, function () {
+
+        let clazz = class {};
+        let parser = (ctx) => {};
+        let fn = bodyParser(parser);
+
+        fn(clazz);
+
+        assert.equal(parser, clazz[config.bodyParserProp]);
+      });
+    });
+    describe('method', function () {
+      it(`should set the correct value for [${Symbol.keyFor(config.bodyParserProp)}] property on method`, function () {
+
+        let clazz = class {};
+        let descriptor = {
+          value: function () {}
+        };
+        let parser = (ctx) => {};
+        let fn = bodyParser(parser);
+
+        fn(clazz, 'any', descriptor);
+
+        assert.notProperty(clazz, config.bodyParser);
+        assert.deepEqual(parser, descriptor.value[config.bodyParserProp]);
+      });
+    });
+  }); 
+  
+  describe('@consumes', function () {
+    describe('class', function () {
+      it(`should set the correct value for [${Symbol.keyFor(config.consumesProp)}] property on class`, function () {
+
+        let clazz = class {};
+        let mimeTypes = ['text/plain', 'application/json', 'xml'];
+        let fn = consumes(...mimeTypes);
+
+        fn(clazz);
+
+        assert.deepEqual(mimeTypes, clazz[config.consumesProp]);
+      });
+    });
+    describe('method', function () {
+      it(`should set the correct value for [${Symbol.keyFor(config.consumesProp)}] property on method`, function () {
+
+        let clazz = class {};
+        let descriptor = {
+          value: function () {}
+        };
+        let mimeTypes = [];
+        let fn = consumes(...mimeTypes);
+
+        fn(clazz, 'any', descriptor);
+
+        assert.notProperty(clazz, config.consumesProp);
+        assert.deepEqual(mimeTypes, descriptor.value[config.consumesProp]);
+      });
+    });
+  });
+
+  describe('@controller', function () {
+    it(`should set the correct value for [${Symbol.keyFor(config.controllerTag)}] property on class`, function () {
+
+      let clazz = class clazz{};
+      
+      controller(clazz);
+
+      assert.propertyVal(clazz, config.controllerTag, true);
+    });
+    it(`should throw on anonymous class`, function () {
+
+      let clazz = class {};
+      let fn = () => controller(clazz);
+      
+      assert.throws(fn, 'Anonymous controllers not supported');
+    });
+  });
+  
+  describe('@produces', function () {
+    describe('class', function () {
+      it(`should set the correct value for [${Symbol.keyFor(config.producesProp)}] property on class`, function () {
+
+        let clazz = class {};
+        let mimeTypes = ['text/plain', 'application/json', 'xml'];
+        let fn = produces(...mimeTypes);
+
+        fn(clazz);
+
+        assert.deepEqual(mimeTypes, clazz[config.producesProp]);
+      });
+    });
+    describe('method', function () {
+      it(`should set the correct value for [${Symbol.keyFor(config.producesProp)}] property on method`, function () {
+
+        let clazz = class {};
+        let descriptor = {
+          value: function () { }
+        };
+        let mimeTypes = [];
+        let fn = produces(...mimeTypes);
+
+        fn(clazz, 'any', descriptor);
+
+        assert.notProperty(clazz, config.producesProp);
+        assert.deepEqual(mimeTypes, descriptor.value[config.producesProp]);
+      });
+    });
+  });
+  
+  describe('@queryParser', function () {
+    describe('class', function () {
+      it(`should set the correct value for [${Symbol.keyFor(config.queryParserProp)}] property on class`, function () {
+
+        let clazz = class {};
+        let parser = (str) => {};
+        let fn = queryParser(parser);
+
+        fn(clazz);
+
+        assert.equal(parser, clazz[config.queryParserProp]);
+      });
+    });
+    describe('method', function () {
+      it(`should set the correct value for [${Symbol.keyFor(config.queryParserProp)}] property on method`, function () {
+
+        let clazz = class {};
+        let descriptor = {
+          value: function () {}
+        };
+        let parser = (str) => {};
+        let fn = queryParser(parser);
+
+        fn(clazz, 'any', descriptor);
+
+        assert.notProperty(clazz, config.queryParser);
+        assert.deepEqual(parser, descriptor.value[config.queryParserProp]);
+      });
+    });
+  });  
+ 
+});
+
---
+++ test/router.js
@@ -0,0 +1,423 @@
+'use strict';
+
+import { assert } from 'chai';
+
+import * as router from '../lib/router';
+
+describe('router', function() {
+	
+	describe('parseRoute', function() {
+		it('must return undefined when path not provided', function() {
+			
+			const pathsList = [ undefined, null, ''];	
+            	
+			let actual;
+			
+			for (let path of pathsList) {
+                	
+				actual = router.parseRoute(path);
+		
+				assert.isUndefined(actual);
+			}
+		});	
+		it('must return the correct pattern and group names', function() {
+			
+			const sample = [ 
+                {
+                    path: '/users/anonymous/page/default',
+                    caseSensitive: false,
+                    strict: false,
+                    
+                    expected: {
+                        pattern: new RegExp(`^/users/anonymous/page/default$`, 'i'),
+                        groupNames: []
+                    },
+                },
+                {
+                    path: '/users/`userId`/`page`/default/',
+                    caseSensitive: false,
+                    strict: false,
+                    
+                    expected: {
+                        pattern: new RegExp(`^/users/${router.defaultPathPattern}/${router.defaultPathPattern}/default$`, 'i'),
+                        groupNames: ['userId', 'page']
+                    },
+                },
+                {
+                    path: '/users/`userId`/`page`/default',
+                    caseSensitive: true,
+                    strict: false,
+                    
+                    expected: {
+                        pattern: new RegExp(`^/users/${router.defaultPathPattern}/${router.defaultPathPattern}/default$`),
+                        groupNames: ['userId', 'page']
+                    }
+                },
+                {
+                    path: '/users/`userId`/`page`/default',
+                    caseSensitive: false,
+                    strict: true,
+                    
+                    expected: {
+                        pattern: new RegExp(`^/users/${router.defaultPathPattern}/${router.defaultPathPattern}/default$`, 'i'),
+                        groupNames: ['userId', 'page']
+                    }
+                },
+                {
+                    path: '/users/`userId`/`page`/default/',
+                    caseSensitive: true,
+                    strict: true,
+                    
+                    expected: {
+                        pattern: new RegExp(`^/users/${router.defaultPathPattern}/${router.defaultPathPattern}/default/$`),
+                        groupNames: ['userId', 'page']
+                    }
+                },
+                {
+                    path: '/users/`userId \\d+`/`page`/default',
+                    caseSensitive: false,
+                    strict: false,
+                    
+                    expected: {
+                        pattern: new RegExp(`^/users/(\\d+)/${router.defaultPathPattern}/default$`, 'i'),
+                        groupNames: ['userId', 'page']
+                    },
+                },   
+                {
+                    path: '/users/`userId \\d+`/`page \\w{8, 10}`/default',
+                    caseSensitive: false,
+                    strict: false,
+                    
+                    expected: {
+                        pattern: new RegExp(`^/users/(\\d+)/(\\w{8, 10})/default$`, 'i'),
+                        groupNames: ['userId', 'page']
+                    },
+                }, 
+                {
+                    path: '/users/`userId \\d+`/view-`page \\w{8, 10}`/default',
+                    caseSensitive: false,
+                    strict: false,
+                    
+                    expected: {
+                        pattern: new RegExp(`^/users/(\\d+)/view-(\\w{8, 10})/default$`, 'i'),
+                        groupNames: ['userId', 'page']
+                    }
+                }
+            ];		
+
+            let actual;
+            
+			for (let item of sample) {
+                	
+				actual = router.parseRoute(item.path, item.caseSensitive, item.strict);
+                		
+                assert.equal(actual.pattern.source, item.expected.pattern.source);
+                assert.equal(actual.pattern.flags, item.expected.pattern.flags);
+				assert.sameMembers(actual.groupNames, item.expected.groupNames);
+			}
+		});		
+	});
+
+	describe('findRoute', function() {
+		it('must return undefined when routes not provided', function() {
+			
+            const appSettings = {};
+			const routesList = [ undefined, null, []];	
+            const request = {};
+            	
+			let actual;
+			
+			for (let routes of routesList) {
+                	
+				actual = router.findRoute(appSettings, routes, request);
+		
+				assert.isUndefined(actual);
+			}
+		});	
+		it('must return undefined when request url not provided', function() {
+			
+            const appSettings = {};
+			const routes = [{}];	
+            const requestsList = [ undefined, null, {}];
+            	
+			let actual;
+			
+			for (let request of requestsList) {
+                	
+				actual = router.findRoute(appSettings, routes, request);
+		
+				assert.isUndefined(actual);
+			}
+		});	
+		it('must return undefined when route not found', function() {
+			
+            const appSettings = {
+                route: {
+                    strict: false
+                }
+            };
+			const routes = [
+                {
+                    pattern: new RegExp(`^/users/(\\d+)/view-(\\w{7,9})$`, 'i'),
+                    groupNames: ['userId', 'page'],
+                    httpMethods: [ 'get' ]
+                },
+                {
+                    pattern: new RegExp(`^/users/(\\d+)/(\\w{7,9})$`, 'i'),
+                    groupNames: ['userId', 'page'],
+                    httpMethods: [ 'get' ]
+                },
+                {
+                    pattern: new RegExp(`^/users/(\\d+)/view-(\\w{7,9})$`, 'i'),
+                    groupNames: ['userId', 'page'],
+                    httpMethods: [ 'post' ]
+                },
+                {
+                    pattern: new RegExp(`^/usersList/(\\d+)/view-(\\w{7,9})$`),
+                    groupNames: ['userId', 'page'],
+                    httpMethods: [ 'get' ]
+                }
+            ];	
+            const request = {
+                    method: 'POST',
+                    url: 'http://www.allmy.sites.com/users/649/view-settingsSection',
+                    headers: { }
+                };
+            	
+            const actual = router.findRoute(appSettings, routes, request);
+    
+            assert.isUndefined(actual);
+		});		
+		it('must match the correct route', function() {
+			
+            const appSettings = {
+                route: {
+                    strict: false
+                }
+            };
+			const routes = [
+                {
+                    pattern: new RegExp(`^/users/(\\d+)/view-(\\w{7,9})$`, 'i'),
+                    groupNames: ['userId', 'page'],
+                    httpMethods: [ 'post' ]
+                },
+                {
+                    pattern: new RegExp(`^/usersList/(\\d+)/view-(\\w{7,9})$`, 'i'),
+                    groupNames: ['userId', 'page'],
+                    httpMethods: [ 'get' ]
+                },
+                {
+                    pattern: new RegExp(`^/users/(\\d+)/view-(\\w{7,9})$`, 'i'),
+                    groupNames: ['userId', 'page'],
+                    httpMethods: [ 'get' ]
+                },
+                {
+                    pattern: new RegExp(`^/usersList/(\\d+)/view-(\\w{7,9})$`, 'i'),
+                    groupNames: ['userId', 'page'],
+                    httpMethods: [ 'post' ]
+                }
+            ];	
+            const request = {
+                method: 'GET',
+                url: 'http://www.allmy.sites.com/users/649/view-settings',
+                headers: { }
+            };
+            	
+            const actual = router.findRoute(appSettings, routes, request);
+    
+            assert.isObject(actual);
+            assert.deepEqual(actual.route, routes[2]);
+            
+            const actualParamsKeys = Object.keys(actual.params);
+            
+            assert.deepEqual(actualParamsKeys, ['userId', 'page']);
+            assert.equal(actual.params['userId'], 649);
+            assert.equal(actual.params['page'], 'settings');
+		});
+		it('must return undefined when content type not allowed', function() {
+			
+            const appSettings = {
+                route: {
+                    strict: false
+                }
+            };
+			const routes = [
+                {
+                    pattern: new RegExp(`^/users/(\\d+)/view-(\\w{7,9})$`, 'i'),
+                    groupNames: ['userId', 'page'],
+                    httpMethods: [ 'get' ],
+                    consumes: ['application/json']
+                }
+            ];	
+            const request = {
+                method: 'GET',
+                url: 'http://www.allmy.sites.com/users/649/view-settings',
+                headers: {
+                    'content-type': 'text/html',
+                    'transfer-encoding': 'chunked'
+                }
+            };
+            	
+            const actual = router.findRoute(appSettings, routes, request);
+    
+            assert.isUndefined(actual);
+		});
+		it('must match the correct route when content type is allowed', function() {
+			
+            const appSettings = {
+                route: {
+                    strict: false
+                }
+            };
+			const routes = [
+                {
+                    pattern: new RegExp(`^/users/(\\d+)/view-(\\w{7,9})$`, 'i'),
+                    groupNames: ['userId', 'page'],
+                    httpMethods: [ 'get' ],
+                    consumes: ['text/html']
+                }
+            ];	
+            const request = {
+                method: 'GET',
+                url: 'http://www.allmy.sites.com/users/649/view-settings',
+                headers: {
+                    'content-type': 'text/html',
+                    'transfer-encoding': 'chunked'
+                }
+            };
+            
+            const actual = router.findRoute(appSettings, routes, request);
+    
+            assert.isObject(actual);
+            assert.deepEqual(actual.route, routes[0]);
+            
+            const actualParamsKeys = Object.keys(actual.params);
+            
+            assert.deepEqual(actualParamsKeys, ['userId', 'page']);
+            assert.equal(actual.params['userId'], 649);
+            assert.equal(actual.params['page'], 'settings');
+		});
+		it('must return undefined when accept not allowed', function() {
+			
+            const appSettings = {
+                route: {
+                    strict: false
+                }
+            };
+			const routes = [
+                {
+                    pattern: new RegExp(`^/users/(\\d+)/view-(\\w{7,9})$`, 'i'),
+                    groupNames: ['userId', 'page'],
+                    httpMethods: [ 'get' ],
+                    produces: ['application/json']
+                }
+            ];	
+            const request = {
+                method: 'GET',
+                url: 'http://www.allmy.sites.com/users/649/view-settings',
+                headers: {
+                    'accept': 'text/html'
+                }
+            };
+            	
+            const actual = router.findRoute(appSettings, routes, request);
+    
+            assert.isUndefined(actual);
+		});
+		it('must match the correct route when accept is allowed', function() {
+			
+            const appSettings = {
+                route: {
+                    strict: false
+                }
+            };
+			const routes = [
+                {
+                    pattern: new RegExp(`^/users/(\\d+)/view-(\\w{7,9})$`, 'i'),
+                    groupNames: ['userId', 'page'],
+                    httpMethods: [ 'get' ],
+                    produces: ['text/html']
+                }
+            ];	
+            const request = {
+                method: 'GET',
+                url: 'http://www.allmy.sites.com/users/649/view-settings',
+                headers: {
+                    'accept': 'text/html'
+                }
+            };
+            	
+            const actual = router.findRoute(appSettings, routes, request);
+    
+            assert.isObject(actual);
+            assert.deepEqual(actual.route, routes[0]);
+            
+            const actualParamsKeys = Object.keys(actual.params);
+            
+            assert.deepEqual(actualParamsKeys, ['userId', 'page']);
+            assert.equal(actual.params['userId'], 649);
+            assert.equal(actual.params['page'], 'settings');
+		});
+		it('must return undefined when accept-version not allowed', function() {
+			
+            const appSettings = {
+                route: {
+                    strict: false
+                }
+            };
+			const routes = [
+                {
+                    pattern: new RegExp(`^/users/(\\d+)/view-(\\w{7,9})$`, 'i'),
+                    groupNames: ['userId', 'page'],
+                    httpMethods: [ 'get' ],
+                    version: '3.1.6'
+                }
+            ];	
+            const request = {
+                method: 'GET',
+                url: 'http://www.allmy.sites.com/users/649/view-settings',
+                headers: {
+                    'accept-version': '>3.2.5'
+                }
+            };
+            	
+            const actual = router.findRoute(appSettings, routes, request);
+    
+            assert.isUndefined(actual);
+		});
+		it('must match the correct route when accept-version is allowed', function() {
+			
+            const appSettings = {
+                route: {
+                    strict: false
+                }
+            };
+			const routes = [
+                {
+                    pattern: new RegExp(`^/users/(\\d+)/view-(\\w{7,9})$`, 'i'),
+                    groupNames: ['userId', 'page'],
+                    httpMethods: [ 'get' ],
+                    version: '3.5.6'
+                }
+            ];	
+            const request = {
+                method: 'GET',
+                url: 'http://www.allmy.sites.com/users/649/view-settings',
+                headers: {
+                    'accept-version': '>3.2.5'
+                }
+            };
+            	
+            const actual = router.findRoute(appSettings, routes, request);
+    
+            assert.isObject(actual);
+            assert.deepEqual(actual.route, routes[0]);
+            
+            const actualParamsKeys = Object.keys(actual.params);
+            
+            assert.deepEqual(actualParamsKeys, ['userId', 'page']);
+            assert.equal(actual.params['userId'], 649);
+            assert.equal(actual.params['page'], 'settings');
+		});
+    });	
+});
